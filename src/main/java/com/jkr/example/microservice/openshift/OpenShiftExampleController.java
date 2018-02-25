db8003515d009d0c85668847166de5efcd1b0ccd
kayjay1980 <36807890+kayjay1980@users.noreply.github.com>
2018-02-25 01:29:46 -0500

    Update OpenShiftExampleController.java

--- src/main/java/com/jkr/example/microservice/openshift/OpenShiftExampleController.java
+++ src/main/java/com/jkr/example/microservice/openshift/OpenShiftExampleController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @EnableAutoConfiguration
 public class OpenShiftExampleController {
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+	@RequestMapping(value = "/crypto/prices", method = RequestMethod.GET, produces = "application/json")
 	Map<String,String> getPrice() {
 		
 		Map<String,String> priceMap = new HashMap<String,String>();
